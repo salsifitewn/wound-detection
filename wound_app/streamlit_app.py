08cfac6233181c1c8d96b5f3060bf79849047fb6
salsifitewn <salsifite@gmail.com>
2024-03-27 00:05:07 +0100

    feat: add multiple files upload

--- wound_app/streamlit_app.py
+++ wound_app/streamlit_app.py
@@ -6,22 +6,23 @@ import streamlit as st
 processor = ViTImageProcessor.from_pretrained('Hemg/Wound-classification')
 model = ViTForImageClassification.from_pretrained('Hemg/Wound-classification')
 def main():
-   uploaded_file = st.file_uploader("Upload image",type=["png", "jpg", "jpeg"] )
-   if uploaded_file is not None:
-      bytes_data = uploaded_file.getvalue()
-      st.write("filename:", uploaded_file.name)
-      image = Image.open(uploaded_file).convert("RGB")
+   uploaded_file = st.file_uploader("Upload image",type=["png", "jpg", "jpeg"],multiple_files=True )
+   for uploaded_file in uploaded_file:
+      if uploaded_file is not None:
+         bytes_data = uploaded_file.getvalue()
+         st.write("filename:", uploaded_file.name)
+         image = Image.open(uploaded_file).convert("RGB")
 
 
-      inputs = processor(images=image, return_tensors="pt")
-      outputs = model(**inputs)
-      logits = outputs.logits
-      predicted_class_idx = logits.argmax(-1).item()
-      print("Predicted class:", GoogleTranslator(source='auto', target='fr').translate(model.config.id2label[predicted_class_idx]))
-      print("Predicted class:",model.config.id2label[predicted_class_idx])
-      st.image(bytes_data)
-      st.text('Predicted class:' + GoogleTranslator(source='auto', target='fr').translate(model.config.id2label[predicted_class_idx]))
-      st.text('Predicted class:' + model.config.id2label[predicted_class_idx])
+         inputs = processor(images=image, return_tensors="pt")
+         outputs = model(**inputs)
+         logits = outputs.logits
+         predicted_class_idx = logits.argmax(-1).item()
+         print("Predicted class:", GoogleTranslator(source='auto', target='fr').translate(model.config.id2label[predicted_class_idx]))
+         print("Predicted class:",model.config.id2label[predicted_class_idx])
+         st.image(bytes_data)
+         st.text('Predicted class:' + GoogleTranslator(source='auto', target='fr').translate(model.config.id2label[predicted_class_idx]))
+         st.text('Predicted class:' + model.config.id2label[predicted_class_idx])
 
 if __name__ == "__main__":
     main()
